539b97b8cf36dac9107c2597a414a4fb1d51adc9
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2026-02-27 18:49:33 +0600

    Get reposiotry complete.

---
+++ lib/core/network/api_endpoints.dart
@@ -0,0 +1,4 @@
+abstract final class ApiEndpoints {
+  static const String baseUrl = 'https://api.antripe.com/';
+  static const String homepageDataEndpoint = 'v1/contact/api.json';
+}
--- lib/features/home/data/repositories/home_reposiotry.dart
+++ lib/features/home/data/repositories/home_reposiotry.dart
@@ -1,3 +1,27 @@
+import 'dart:convert';
+
+import 'package:qunova/core/network/api_endpoints.dart';
+import 'package:qunova/features/home/data/models/response_data_model.dart';
+import 'package:http/http.dart' as http;
+
 class HomeReposiotry {
-  
+  Future<BackendResModel> fetchData() async {
+    http.Response? response;
+    Map<String, String> requestHeaders = {"Content-Type": "application/json"};
+    try {
+      response = await http.get(
+        Uri.parse(
+          '${ApiEndpoints.baseUrl}${ApiEndpoints.homepageDataEndpoint}',
+        ),
+      );
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+    if (response.statusCode == 200) {
+      print(jsonDecode(response.body));
+      return BackendResModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to get data');
+    }
+  }
 }
